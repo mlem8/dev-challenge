031e7f36902e84b8a21b8c39b6c9241b188e949b
mlem8 <matthew.lem@cinemassive.com>
2016-07-04 12:56:29 -0400

    moving movie description component into MovieDescription.js

---
+++ src/routes/movies/MovieDescription.js
@@ -0,0 +1,47 @@
+import React, { PropTypes } from 'react';
+import Well from 'react-bootstrap/lib/Well';
+import classNames from 'classnames/bind';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Movies.css';
+
+let cx = classNames.bind(s);
+
+var MovieDescription = React.createClass( {
+
+  getInitialState: function(){
+    return { collapsed: true };
+  },
+
+  // collapse when new movie selected
+  componentWillReceiveProps : function() {
+    this.setState({collapsed: true});
+  },
+
+  handleClick: function() {
+    this.setState({collapsed:!this.state.collapsed});
+  },
+
+  render: function() {
+
+    let descriptionClass = cx({
+      movieDescription: true,
+      collapsed: this.state.collapsed
+    });
+    let collapseClass = cx({
+      readMore: true,
+      hidden: this.props.description.length < 292
+    });
+    let linkText = this.state.collapsed ? "More" : "Less";
+
+    return (
+      <Well bsSize="large">
+        <p className={descriptionClass}>{this.props.description}</p>
+        <a href="javascript:void(0)" className={collapseClass} onClick={this.handleClick}>{linkText}</a>
+      </Well>
+    );
+
+  }
+
+});
+
+export default withStyles(s)(MovieDescription);
