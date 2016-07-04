50c737fe359d4c626aae3c9da61e584210f313d4
mlem8 <matthew.lem@cinemassive.com>
2016-07-04 16:41:36 -0400

    moving movie table component to MovieTable.js

--- src/routes/movies/MovieList.js
+++ src/routes/movies/MovieList.js
@@ -1,10 +1,10 @@
 import React, { PropTypes } from 'react';
-import Table from 'react-bootstrap/lib/Table';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Movies.css';
 
 // Child components
 import MovieSearch from './MovieSearch';
+import MovieTable from './MovieTable';
 
 var MovieList = React.createClass({
 
@@ -22,36 +22,19 @@ var MovieList = React.createClass({
 
   render: function() {
 
-    var movies = this.props.items,
-      searchString = this.state.searchString.trim().toLowerCase();
-
-    if(searchString.length > 0){
-
-      movies = movies.filter(function(l){
-        return l.TitleName.toLowerCase().match( searchString );
-      });
-    }
-
     return (
 
-    <div>
-      <MovieSearch handleChange={this.handleChange} searchString={this.state.searchString} />
-      <Table responsive>
-        <thead>
-        <tr>
-          <th>Title</th>
-          <th>Released</th>
-        </tr>
-        </thead>
-        <tbody>
-        { movies.map(function(m, index){
-          let movieItemClass = s.movieItem;
-          {if (this.props.selectedMovie === m) {movieItemClass = s.selected}}
-          var boundClick = this.handleClick.bind(this, m);
-          return <tr key={index} className={movieItemClass}><td onClick={boundClick}>{m.TitleName}</td><td>{m.ReleaseYear}</td></tr>}, this) }
-        </tbody>
-      </Table>
-    </div>
+      <div>
+        <MovieSearch
+          handleChange={this.handleChange}
+          searchString={this.state.searchString}
+        />
+        <MovieTable
+          movies={this.props.items}
+          handleClick={this.handleClick}
+          searchString={this.state.searchString}
+        />
+      </div>
 
     );
 
---
+++ src/routes/movies/MovieTable.js
@@ -0,0 +1,49 @@
+import React, { PropTypes } from 'react';
+import Table from 'react-bootstrap/lib/Table';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Movies.css';
+
+var MovieTable = React.createClass({
+
+  handleClick: function (obj) {
+    this.props.handleClick(obj);
+  },
+
+  render: function() {
+
+    var movies = this.props.movies,
+      searchString = this.props.searchString.trim().toLowerCase();
+
+    if(searchString.length > 0){
+
+      movies = movies.filter(function(m){
+        return m.TitleName.toLowerCase().match( searchString );
+      });
+    }
+
+    return (
+
+      <div>
+        <Table responsive>
+          <thead>
+          <tr>
+            <th>Title</th>
+            <th>Released</th>
+          </tr>
+          </thead>
+          <tbody>
+          { movies.map(function(m, index){
+            let movieItemClass = s.movieItem;
+            {if (this.props.selectedMovie === m) {movieItemClass = s.selected}}
+            var boundClick = this.handleClick.bind(this, m);
+            return <tr key={index} className={movieItemClass}><td onClick={boundClick}>{m.TitleName}</td><td>{m.ReleaseYear}</td></tr>}, this) }
+          </tbody>
+        </Table>
+      </div>
+
+    );
+
+  }
+});
+
+export default withStyles(s)(MovieTable);
