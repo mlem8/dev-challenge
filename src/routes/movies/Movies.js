90d7a559c0ef6f08d9d4cbdf56f004f8b6a7075c
mlem8 <matthew.lem@cinemassive.com>
2016-07-06 16:38:37 -0400

    moving exampleMovie to own json file

--- src/routes/movies/Movies.js
+++ src/routes/movies/Movies.js
@@ -13,26 +13,12 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Movies.css';
+import exampleMovie from './exampleMovie.json';
 
 // Child components
 import Details from './details/Details';
 import List from './list/List';
 
-const EXAMPLE_MOVIE = {
-  TitleName: 'Major Payne', ReleaseYear: 1995, TitleId: 0,
-  Storylines: [{
-    Description:
-    'Major Benson Winifred Payne is being discharged from the Marines. Payne ' +
-    'is a killin\' machine, but the wars of the world are no longer fought on the ' +
-    'battlefield. A career Marine, he has no idea what to do as a civilian, so his ' +
-    'commander finds him a job - commanding officer of a local school\'s JROTC ' +
-    'program, a bunch or ragtag losers with no hope. Using such teaching tools as ' +
-    'live grenades and real bullets, Payne starts to instill the corp with some hope. ' +
-    'But when Payne is recalled to fight in Bosnia, will he leave the corp that has just ' +
-    'started to believe in him, or will he find out that killin\' ain\'t much of a livin\'?',
-  }],
-  Participants: [{Name:'Damon Wayans'}], Awards: []
-};
 
 class Movies extends Component {
 
@@ -48,7 +34,7 @@ class Movies extends Component {
   };
 
   state = {
-    selectedMovie: EXAMPLE_MOVIE,
+    selectedMovie: exampleMovie,
   };
 
   handleSelect = (movie) => {
